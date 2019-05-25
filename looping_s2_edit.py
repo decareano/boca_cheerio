6ad324b65c79d2b5f39814adf94af96ab4401393
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-05-25 11:36:10 -0700

    added an edit file

--- looping_s2_edit.py
+++ looping_s2_edit.py
@@ -8,15 +8,25 @@ line_no = 1
 while f1_line != '' or f2_line != '':
 	f1_line = f1_line.rstrip()
 	f2_line = f2_line.rstrip()
+	#b_test = 0
+	#c_test = 0
 
+	# if f1_line != f2_line:
+	# 	if f1_line.startswith("Basal-1-2016-"):
+	# 		a_test = f1_line#[12:17]
+	# 	print(a_test)
+	# 	if f1_line.startswith("sc-lib-basal-"):
+	# 		b_test = f1_line#[12:17]
+	# 	print(b_test)
+	# 	if f1_line.startswith("sc-lib-luminal-"):
+	# 		c_test = f1_line#[12:17]
+	# 	print(c_test)
+	# 	# b_test = f2_line[10:17]
+	# 	# print(b_test)
 
+	print("this is: " + f1_line + "\n" + "this is: " + f2_line)
 	if f1_line != f2_line:
-		a_test = f1_line[10:17]
-		print(a_test)
-		b_test = f2_line[10:17]
-		print(b_test)
-		
-		#print("this is: " + f1_line + "\n" + "this is: " + f2_line)
+		print("no match")
 	print()
 
 	# If a line does not exist on file1 then mark the output with + sign
